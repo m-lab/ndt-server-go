66f7909ef025174d3844ef8870d00b5f51e875cf
Greg Russell <gfr10598@gmail.com>
2017-07-26 14:09:59 -0400

    Add deadline, finalize, close port

--- tests/middlebox.go
+++ tests/middlebox.go
@@ -4,10 +4,8 @@ package tests
 import (
 	"crypto/rand"
 	"fmt"
-	"io"
 	"net"
 	"os"
-	"sync"
 	"time"
 
 	"github.com/m-lab/ndt-server-go/protocol"
@@ -16,54 +14,69 @@ import (
 // MiddleBox accepts connection, then writes as fast as possible,
 // for 5 seconds.
 // MSS should be 1456
-// Should send data for 5 seconds, then
-func MiddleBox(lnr net.Listener, wg *sync.WaitGroup) {
-	defer wg.Done()
+// After connection is accepted should:
+//   1. send data for 5 seconds
+//   2. Send results
+//   3. Receive results
+//   4. Finalize
+//   5. Close port
+//   6. Notify waiter.
+func MiddleBox(conn net.Conn, lnr *net.TCPListener, testConn chan net.Conn) {
 	data := make([]byte, 8192)
 	rand.Read(data)
 
-	mb, err := lnr.Accept()
+	mb, err := lnr.AcceptTCP()
 	if err != nil {
 		fmt.Println("Error accepting: ", err.Error())
 		// TODO - should this be fatal?
 		os.Exit(1)
 	}
 
+	// Without this line, the Sleep above causes hangs at end
+	// of send loop.
+	mb.SetWriteBuffer(2 * 8192)
 	// TODO - set up MSS and CWND.
 	fmt.Println("Middlebox connected")
 
-	timeout := time.After(5 * time.Second)
+	start := time.Now()
 	count := 0
-FOR:
-	for {
-		select {
-		case <-timeout:
-			break FOR
-		default:
-			// Send some data.
-			mb.Write(data)
-			count++
+	for time.Now().Before(start.Add(5 * time.Second)) {
+		// Send some data.
+		mb.SetWriteDeadline(time.Now().Add(100 * time.Millisecond))
+		_, err := mb.Write(data)
+		if err != nil {
+			fmt.Println(time.Now().Sub(start), " ", err)
+			break
 		}
+		count++
 	}
 	fmt.Println("Total of ", count, " 8KB blocks sent.")
+	// Send test connection back to be closed.
+	testConn <- mb
 }
 
-func DoMiddleBox(conn io.Writer) {
+func DoMiddleBox(conn net.Conn) {
 	// Handle the MiddleBox test.
-	lnr, err := net.Listen("tcp", "localhost:0")
+	addr, _ := net.ResolveTCPAddr("tcp", "localhost:0")
+
+	lnr, err := net.ListenTCP("tcp", addr)
 	if err != nil {
 		fmt.Println("Error listening:", err.Error())
 		os.Exit(1)
 	}
+	defer lnr.Close()
 
 	_, port, err := net.SplitHostPort(lnr.Addr().String())
-	var wg sync.WaitGroup
-	wg.Add(1)
-	// TODO - is there any real advantage to goroutine here?
-	go MiddleBox(lnr, &wg)
+	connChan := make(chan net.Conn)
+	go MiddleBox(conn, lnr, connChan)
 	// Send the TEST_PREPARE message.
 	protocol.SendJSON(conn, 3, protocol.SimpleMsg{port})
-	wg.Wait()
+	fmt.Println("Waiting for test to complete.")
+	testConn := <-connChan
 	fmt.Println("Middlebox done")
 	protocol.SendJSON(conn, 5, protocol.SimpleMsg{"Results"})
+	msg, err := protocol.ReadMessage(conn)
+	fmt.Println(msg)
+	protocol.Send(conn, 6, []byte{})
+	testConn.Close()
 }
