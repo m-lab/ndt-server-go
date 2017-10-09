5be2f87e5db8d27815e04fb9f50673024d25d208
Greg Russell <gfr10598@gmail.com>
2017-10-09 15:24:43 -0400

    Test that dumps tcpinfo content

--- tcpinfo/tcpinfo_test.go
+++ tcpinfo/tcpinfo_test.go
@@ -12,33 +12,26 @@ import (
 	"github.com/m-lab/ndt-server-go/tcpinfo"
 )
 
-func listen(ln net.Listener, wg *sync.WaitGroup, ch chan net.Conn) {
+func listen(ln net.Listener, wg *sync.WaitGroup) {
 	defer ln.Close()
 
 	log.Println("Listening on ", ln.Addr())
-	tcp, err := ln.Accept()
+	_, err := ln.Accept()
 	if err != nil {
 		fmt.Println("Error accepting: ", err.Error())
 		os.Exit(1)
 	}
-	log.Println("Connected")
 
-	ch <- tcp
-	log.Println("Sent conn")
 	wg.Wait()
 }
 
 func BenchmarkTCPInfo2(b *testing.B) {
-
-	// listen on all interfaces
 	ln, _ := net.Listen("tcp", ":0")
 
 	var wg sync.WaitGroup
 	wg.Add(1)
-	ch := make(chan net.Conn)
-	go listen(ln, &wg, ch)
+	go listen(ln, &wg)
 
-	log.Println("Dialing")
 	dialer, err := net.Dial("tcp", ln.Addr().String())
 	defer dialer.Close()
 	if err != nil {
@@ -46,14 +39,32 @@ func BenchmarkTCPInfo2(b *testing.B) {
 		os.Exit(1)
 	}
 
-	log.Println("Waiting")
-	tcp := <-ch
-	log.Println("Connected on ", tcp.RemoteAddr())
 	var info syscall.TCPInfo
+	b.ResetTimer()
 	for n := 0; n < b.N; n++ {
 		info, _ = tcpinfo.TCPInfo2(dialer.(*net.TCPConn))
 	}
 
 	wg.Done()
 	log.Printf("%+v\n", info)
+}
+
+func TestBasic(t *testing.T) {
+	ln, _ := net.Listen("tcp", ":0")
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go listen(ln, &wg)
+
+	dialer, err := net.Dial("tcp", ln.Addr().String())
+	defer dialer.Close()
+	if err != nil {
+		fmt.Println("Error dialing: ", err.Error())
+		os.Exit(1)
+	}
+
+	var info syscall.TCPInfo
+	info, _ = tcpinfo.TCPInfo2(dialer.(*net.TCPConn))
+	log.Printf("%+v\n", info)
+
 }
