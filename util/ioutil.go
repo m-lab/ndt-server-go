d67810ac7798a239a7e4b9f2d21315a5d5ba39cc
Simone Basso <bassosimone@gmail.com>
2018-01-30 16:53:28 +0100

    ioutil.go: sketch out DeadlineReader

---
+++ util/ioutil.go
@@ -0,0 +1,43 @@
+// Part of ndt-server-go <https://github.com/m-lab/ndt-server-go>, which
+// is free software under the Apache v2.0 License.
+
+package util
+
+import (
+	"io"
+	"net"
+	"time"
+)
+
+// DeadlineReader is a io.Reader where the read has a deadline. See also
+// <https://groups.google.com/forum/#!topic/golang-nuts/afgEYsoV8j0>
+type DeadlineReader struct {
+	io.Reader
+	conn     net.Conn
+	deadline time.Duration
+}
+
+func NewDeadlineReader(conn net.Conn) DeadlineReader {
+	return DeadlineReader{
+		conn:     conn,
+		deadline: 10.0 * time.Second,
+	}
+}
+
+func (dr DeadlineReader) IoReadFull(data []byte) (int, error) {
+	count := 0
+	err := dr.conn.SetReadDeadline(time.Now().Add(dr.deadline))
+	if err != nil {
+		return count, err
+	}
+	count, err = io.ReadFull(dr.Reader, data)
+	if err != nil {
+		return count, err
+	}
+	err = dr.conn.SetReadDeadline(time.Time{})
+	return count, err
+}
+
+func (dr DeadlineReader) SetDeadline(deadline time.Duration) {
+	dr.deadline = deadline
+}
