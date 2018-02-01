ae470380875c7f8b0f610f0797ac835a800ae795
Simone Basso <bassosimone@gmail.com>
2018-02-01 14:56:39 +0100

    simplify the code wrapping net.Conn

--- util/network.go
+++ util/network.go
@@ -4,100 +4,66 @@
 package util
 
 import (
-	"bufio"
 	"errors"
-	"io"
 	"net"
 	"time"
 )
 
-// DeadlineReader is a io.Reader bound to net.Conn. In this reader, the io.Read
-// operation will fail if the operation itself takes more than a specific
-// timeout to complete. Use SetTimeout to control such timeout.
-type DeadlineReader struct {
-	io.Reader
-	conn    net.Conn
+// DeadlineConn is a net.Conn where I/O operations have deadlines. They will
+// fail with an error if it takes more than a specific timeout to complete them.
+type DeadlineConn struct {
+	net.Conn
 	timeout time.Duration
 }
 
-// DefaultReaderTimeout is the default timeout used by a DeadlineReader.
-const DefaultReaderTimeout = 10.0 * time.Second
+// DefaultDeadlineConnTimeout is the default timeout used by DeadlineConn.
+const DefaultDeadlineConnTimeout = 10.0 * time.Second
 
-// NewDeadlineReader creates a DeadlineReader bound to |conn|.
-func NewDeadlineReader(conn net.Conn) DeadlineReader {
-	return DeadlineReader{
-		Reader:  bufio.NewReader(conn),
-		conn:    conn,
-		timeout: DefaultReaderTimeout,
+// NewDeadlineConn creates a new DeadlineConn.
+func NewDeadlineConn(conn net.Conn) DeadlineConn {
+	return DeadlineConn{
+		Conn: conn,
+		timeout: DefaultDeadlineConnTimeout,
 	}
 }
 
 // InvalidTimeoutError is returned when you attempt to set an invalid timeout.
 var InvalidTimeoutError = errors.New("Timeout is invalid")
 
-// SetTimeout sets the DeadlineReader timeout. Negative and zero timeouts cause
+// SetTimeout sets the DeadlineConn timeout. Negative and zero timeouts cause
 // an InvalidTimeoutError to be retured by this function.
-func (dr DeadlineReader) SetTimeout(timeout time.Duration) error {
+func (dc DeadlineConn) SetTimeout(timeout time.Duration) error {
 	if timeout <= 0 {
 		return InvalidTimeoutError
 	}
-	dr.timeout = timeout
+	dc.timeout = timeout
 	return nil
 }
 
-// Read implements reading with a specific timeout.
-func (dr DeadlineReader) Read(data []byte) (int, error) {
+// Read implements net.Conn.Read with a specific timeout.
+func (dc DeadlineConn) Read(data []byte) (int, error) {
 	count := 0
-	err := dr.conn.SetReadDeadline(time.Now().Add(dr.timeout))
+	err := dc.Conn.SetReadDeadline(time.Now().Add(dc.timeout))
 	if err != nil {
 		return count, err
 	}
-	count, err = dr.Reader.Read(data)
+	count, err = dc.Conn.Read(data)
 	if err != nil {
 		return count, err
 	}
-	return count, dr.conn.SetReadDeadline(time.Time{})
+	return count, dc.Conn.SetReadDeadline(time.Time{})
 }
 
-// DeadlineWriter is like DeadlineReader but for write operations.
-type DeadlineWriter struct {
-	io.Writer
-	conn    net.Conn
-	timeout time.Duration
-}
-
-// DefaultWriterTimeout is the default timeout used by a DeadlineWriter.
-const DefaultWriterTimeout = 10.0 * time.Second
-
-// NewDeadlineWriter creates a DeadlineWriter bound to |conn|.
-func NewDeadlineWriter(conn net.Conn) DeadlineWriter {
-	return DeadlineWriter{
-		Writer:  bufio.NewWriter(conn),
-		conn:    conn,
-		timeout: DefaultWriterTimeout,
-	}
-}
-
-// SetTimeout sets the DeadlineWriter timeout. Negative and zero timeouts cause
-// an InvalidTimeoutError to be retured by this function.
-func (dw DeadlineWriter) SetTimeout(timeout time.Duration) error {
-	if timeout <= 0 {
-		return InvalidTimeoutError
-	}
-	dw.timeout = timeout
-	return nil
-}
-
-// Read implements writing with a specific timeout.
-func (dw DeadlineWriter) Write(data []byte) (int, error) {
+// Write implements net.Conn.Write with a specific timeout.
+func (dc DeadlineConn) Write(data []byte) (int, error) {
 	count := 0
-	err := dw.conn.SetWriteDeadline(time.Now().Add(dw.timeout))
+	err := dc.Conn.SetWriteDeadline(time.Now().Add(dc.timeout))
 	if err != nil {
 		return count, err
 	}
-	count, err = dw.Writer.Write(data)
+	count, err = dc.Conn.Write(data)
 	if err != nil {
 		return count, err
 	}
-	return count, dw.conn.SetWriteDeadline(time.Time{})
+	return count, dc.Conn.SetWriteDeadline(time.Time{})
 }
--- util/network_test.go
+++ util/network_test.go
@@ -10,37 +10,33 @@ import (
 	"time"
 )
 
-// MockedConn mocks net.Conn.
-type MockedConn struct {
+// mockedConn: mocks net.Conn
+
+type mockedConn struct {
+	net.Conn
 }
 
-// Read mocks net.Conn.Read.
-func (c MockedConn) Read(b []byte) (int, error) {
+func (mockedConn) Read(b []byte) (int, error) {
 	return len(b), nil
 }
 
-// Write mocks net.Conn.Write.
-func (c MockedConn) Write(b []byte) (int, error) {
+func (mockedConn) Write(b []byte) (int, error) {
 	return len(b), nil
 }
 
-// Close mocks net.Conn.Close.
-func (c MockedConn) Close() error {
+func (mockedConn) Close() error {
 	return nil
 }
 
-// LocalAddr mocks net.Conn.LocalAddr.
-func (c MockedConn) LocalAddr() net.Addr {
+func (mockedConn) LocalAddr() net.Addr {
 	return nil // note: for now not required
 }
 
-// RemoteAddr mocks net.Conn.RemoteAddr.
-func (c MockedConn) RemoteAddr() net.Addr {
+func (mockedConn) RemoteAddr() net.Addr {
 	return nil // note: for now not required
 }
 
-// SetDeadline mocks net.Conn.SetDeadline.
-func (c MockedConn) SetDeadline(t time.Time) error {
+func (c mockedConn) SetDeadline(t time.Time) error {
 	err := c.SetReadDeadline(t)
 	if err == nil {
 		err = c.SetWriteDeadline(t)
@@ -48,250 +44,176 @@ func (c MockedConn) SetDeadline(t time.Time) error {
 	return err
 }
 
-// SetReadDeadline mocks net.Conn.SetReadDeadline.
-func (c MockedConn) SetReadDeadline(t time.Time) error {
+func (mockedConn) SetReadDeadline(t time.Time) error {
 	return nil
 }
 
-// SetWriteDeadline mocks net.Conn.SetWriteDeadline.
-func (c MockedConn) SetWriteDeadline(t time.Time) error {
+func (mockedConn) SetWriteDeadline(t time.Time) error {
 	return nil
 }
 
-// TestDeadlineReaderSetTimeoutWorks ensures that DeadlineReader's SetTimeout
-// returns error if passed a negative timeout and nil otherwise.
-func TestDeadlineReaderSetTimeoutWorks(t *testing.T) {
-	dr := NewDeadlineReader(MockedConn{})
-	err := dr.SetTimeout(-1)
+// Test: setTimeout
+
+func TestDeadlineConnSetTimeoutWorks(t *testing.T) {
+	dc := NewDeadlineConn(mockedConn{})
+	err := dc.SetTimeout(-1)
 	if err != InvalidTimeoutError {
 		t.Error("we should not be able to set a negative timeout")
 	}
-	err = dr.SetTimeout(0)
+	err = dc.SetTimeout(0)
 	if err != InvalidTimeoutError {
 		t.Error("we should not be able to set a zero timeout")
 	}
-	err = dr.SetTimeout(1)
+	err = dc.SetTimeout(1)
 	if err != nil {
 		t.Error("we should be able to set a positive timeout")
 	}
 }
 
-// FailSetReadDeadline is a MockedConn where SetReadDeadline fails.
-type FailSetReadDeadline struct {
-	MockedConn
+// Test: Read
+
+type failSetReadDeadline struct {
+	mockedConn
 }
 
-// SetReadDeadlineError is the error returned by a failed SetReadDeadline.
-var SetReadDeadlineError = errors.New("mocked error")
+var setReadDeadlineError = errors.New("set_read_deadline_error")
 
-// SetReadDeadline always returns an error.
-func (c FailSetReadDeadline) SetReadDeadline(t time.Time) error {
-	return SetReadDeadlineError
+func (failSetReadDeadline) SetReadDeadline(t time.Time) error {
+	return setReadDeadlineError
 }
 
-// TestDeadlineReaderSetReadDeadlineErrorHandled ensures that we correctly
-// deal with a SetReadDeadline error.
-func TestDeadlineReaderSetReadDeadlineErrorHandled(t *testing.T) {
-	dr := NewDeadlineReader(FailSetReadDeadline{})
-	data := make([]byte, 128)
-	count, err := dr.Read(data)
-	if count != 0 {
-		t.Error("unexpected count value returned")
-	}
-	if err != SetReadDeadlineError {
-		t.Error("unexpected error returned")
-	}
+type failRead struct {
+	mockedConn
 }
 
-// FailRead is a MockedConn where Read fails.
-type FailRead struct {
-	MockedConn
+var readError = errors.New("read_error")
+
+func (failRead) Read(base []byte) (int, error) {
+	return 0, readError
 }
 
-// ReadError is the error returned by a failed Read.
-var ReadError = errors.New("mocked error")
-
-// Read always returns an error.
-func (c FailRead) Read(b []byte) (int, error) {
-	return 0, ReadError
+type failClearReadDeadline struct {
+	mockedConn
 }
 
-// TestDeadlineReaderReadErrorHandled ensures that we correctly deal with a
-// Read error. It also ensures that the deadline has been changed.
-func TestDeadlineReaderReadErrorHandled(t *testing.T) {
-	dr := NewDeadlineReader(FailRead{})
-	data := make([]byte, 128)
-	count, err := dr.Read(data)
-	if count != 0 {
-		t.Error("unexpected count value returned")
-	}
-	if err != ReadError {
-		t.Error("unexpected error returned")
-	}
-}
+var clearReadDeadlineError = errors.New("clear_read_deadline_error")
 
-// FailResetReadDeadline is a MockedConn where the a SetReadDeadline that
-// passes in a zero read deadline will fail.
-type FailResetReadDeadline struct {
-	MockedConn
-}
-
-// FailResetReadDeadlineError is the error returned by a failed second
-// SetReadDeadline (meaning that the first succeeds).
-var FailResetReadDeadlineError = errors.New("mocked error")
-
-// SetReadDeadline succeeds unless passed a zero deadline.
-func (c FailResetReadDeadline) SetReadDeadline(t time.Time) error {
+func (failClearReadDeadline) SetReadDeadline(t time.Time) error {
 	zero := time.Time{}
 	if t == zero {
-		return FailResetReadDeadlineError
+		return clearReadDeadlineError
 	}
 	return nil
 }
 
-// TestDeadlineReaderFailResetReadDeadlineErrorHandled ensures that we correctly
-// deal with an error in the second SetReadDeadline.
-func TestDeadlineReaderFailResetReadDeadlineErrorHandled(t *testing.T) {
-	dr := NewDeadlineReader(FailResetReadDeadline{})
-	data := make([]byte, 128)
-	count, err := dr.Read(data)
-	if count != len(data) {
-		t.Error("unexpected count value returned")
+func TestDeadlineConnReadWorks(t *testing.T) {
+	{
+		dc := NewDeadlineConn(failSetReadDeadline{})
+		data := make([]byte, 128)
+		count, err := dc.Read(data)
+		if count != 0 || err != setReadDeadlineError {
+			t.Error("unexpected return value")
+		}
 	}
-	if err != FailResetReadDeadlineError {
-		t.Error("unexpected error returned")
-	}
-}
 
-// TestDeadlineReaderWorks make sure that DeadlineReader works.
-func TestDeadlineReaderWorks(t *testing.T) {
-	dr := NewDeadlineReader(MockedConn{})
-	data := make([]byte, 128)
-	count, err := dr.Read(data)
-	if count != len(data) {
-		t.Error("unexpected count value returned")
+	{
+		dc := NewDeadlineConn(failRead{})
+		data := make([]byte, 128)
+		count, err := dc.Read(data)
+		if count != 0 || err != readError {
+			t.Error("unexpected return value")
+		}
 	}
-	if err != nil {
-		t.Error("unexpected error returned")
-	}
-}
 
-// TestDeadlineWriterSetTimeoutWorks ensures that DeadlineWriter's SetTimeout
-// returns error if passed a negative timeout and nil otherwise.
-func TestDeadlineWriterSetTimeoutWorks(t *testing.T) {
-	dw := NewDeadlineWriter(MockedConn{})
-	err := dw.SetTimeout(-1)
-	if err != InvalidTimeoutError {
-		t.Error("we should not be able to set a negative timeout")
+	{
+		dc := NewDeadlineConn(failClearReadDeadline{})
+		data := make([]byte, 128)
+		count, err := dc.Read(data)
+		if count != len(data) || err != clearReadDeadlineError {
+			t.Error("unexpected return value")
+		}
 	}
-	err = dw.SetTimeout(0)
-	if err != InvalidTimeoutError {
-		t.Error("we should not be able to set a zero timeout")
-	}
-	err = dw.SetTimeout(1)
-	if err != nil {
-		t.Error("we should be able to set a positive timeout")
+
+	{
+		dc := NewDeadlineConn(mockedConn{})
+		data := make([]byte, 128)
+		count, err := dc.Read(data)
+		if count != len(data) || err != nil {
+			t.Error("unexpected return value")
+		}
 	}
 }
 
-// FailSetWriteDeadline is a MockedConn where SetWriteDeadline fails.
-type FailSetWriteDeadline struct {
-	MockedConn
+// Test: Write
+
+type failSetWriteDeadline struct {
+	mockedConn
 }
 
-// SetWriteDeadlineError is the error returned by a failed SetWriteDeadline.
-var SetWriteDeadlineError = errors.New("mocked error")
+var setWriteDeadlineError = errors.New("set_write_deadline_error")
 
-// SetWriteDeadline always returns an error.
-func (c FailSetWriteDeadline) SetWriteDeadline(t time.Time) error {
-	return SetWriteDeadlineError
+func (failSetWriteDeadline) SetWriteDeadline(t time.Time) error {
+	return setWriteDeadlineError
 }
 
-// TestDeadlineWriterSetWriteDeadlineErrorHandled ensures that we correctly
-// deal with a SetWriteDeadline error.
-func TestDeadlineWriterSetWriteDeadlineErrorHandled(t *testing.T) {
-	dw := NewDeadlineWriter(FailSetWriteDeadline{})
-	data := make([]byte, 128)
-	count, err := dw.Write(data)
-	if count != 0 {
-		t.Error("unexpected count value returned")
-	}
-	if err != SetWriteDeadlineError {
-		t.Error("unexpected error returned")
-	}
+type failWrite struct {
+	mockedConn
 }
 
-// FailWrite is a MockedConn where Write fails.
-type FailWrite struct {
-	MockedConn
+var writeError = errors.New("write_error")
+
+func (failWrite) Write(base []byte) (int, error) {
+	return 0, writeError
 }
 
-// WriteError is the error returned by a failed Write.
-var WriteError = errors.New("mocked error")
-
-// Write always returns an error.
-func (c FailWrite) Write(b []byte) (int, error) {
-	return 0, WriteError
+type failClearWriteDeadline struct {
+	mockedConn
 }
 
-// TestDeadlineWriterWriteErrorHandled ensures that we correctly deal with a
-// Write error. It also ensures that the deadline has been changed.
-func TestDeadlineWriterWriteErrorHandled(t *testing.T) {
-	dw := NewDeadlineWriter(FailWrite{})
-	data := make([]byte, 128)
-	count, err := dw.Write(data)
-	t.Log(count)
-	if count != 0 {
-		t.Error("unexpected count value returned")
-	}
-	t.Log(err)
-	if err != WriteError {
-		t.Error("unexpected error returned")
-	}
-}
+var clearWriteDeadlineError = errors.New("clear_write_deadline_error")
 
-// FailResetWriteDeadline is a MockedConn where the a SetWriteDeadline that
-// passes in a zero write deadline will fail.
-type FailResetWriteDeadline struct {
-	MockedConn
-}
-
-// FailResetWriteDeadlineError is the error returned by a failed second
-// SetWriteDeadline (meaning that the first succeeds).
-var FailResetWriteDeadlineError = errors.New("mocked error")
-
-// SetWriteDeadline succeeds unless passed a zero deadline.
-func (c FailResetWriteDeadline) SetWriteDeadline(t time.Time) error {
+func (failClearWriteDeadline) SetWriteDeadline(t time.Time) error {
 	zero := time.Time{}
 	if t == zero {
-		return FailResetWriteDeadlineError
+		return clearWriteDeadlineError
 	}
 	return nil
 }
 
-// TestDeadlineWriterFailResetWriteDeadlineErrorHandled ensures that we
-// correctly deal with an error in the second SetWriteDeadline.
-func TestDeadlineWriterFailResetWriteDeadlineErrorHandled(t *testing.T) {
-	dw := NewDeadlineWriter(FailResetWriteDeadline{})
-	data := make([]byte, 128)
-	count, err := dw.Write(data)
-	if count != len(data) {
-		t.Error("unexpected count value returned")
+func TestDeadlineConnWriteWorks(t *testing.T) {
+	{
+		dc := NewDeadlineConn(failSetWriteDeadline{})
+		data := make([]byte, 128)
+		count, err := dc.Write(data)
+		if count != 0 || err != setWriteDeadlineError {
+			t.Error("unexpected return value")
+		}
 	}
-	if err != FailResetWriteDeadlineError {
-		t.Error("unexpected error returned")
-	}
-}
 
-// TestDeadlineWriterWorks make sure that DeadlineWriter works.
-func TestDeadlineWriterWorks(t *testing.T) {
-	dw := NewDeadlineWriter(MockedConn{})
-	data := make([]byte, 128)
-	count, err := dw.Write(data)
-	if count != len(data) {
-		t.Error("unexpected count value returned")
+	{
+		dc := NewDeadlineConn(failWrite{})
+		data := make([]byte, 128)
+		count, err := dc.Write(data)
+		if count != 0 || err != writeError {
+			t.Error("unexpected return value")
+		}
 	}
-	if err != nil {
-		t.Error("unexpected error returned")
+
+	{
+		dc := NewDeadlineConn(failClearWriteDeadline{})
+		data := make([]byte, 128)
+		count, err := dc.Write(data)
+		if count != len(data) || err != clearWriteDeadlineError {
+			t.Error("unexpected return value")
+		}
+	}
+
+	{
+		dc := NewDeadlineConn(mockedConn{})
+		data := make([]byte, 128)
+		count, err := dc.Write(data)
+		if count != len(data) || err != nil {
+			t.Error("unexpected return value")
+		}
 	}
 }
