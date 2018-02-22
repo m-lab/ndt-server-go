74f215ffa1396f9c3e3bb4251593a9f2332805a2
Gregory Russell <gfr@google.com>
2018-02-22 11:30:23 -0500

    move defer after error check

--- tcpinfo/tcpinfo_test.go
+++ tcpinfo/tcpinfo_test.go
@@ -33,11 +33,11 @@ func BenchmarkTCPInfo2(b *testing.B) {
 	go listen(ln, &wg)
 
 	dialer, err := net.Dial("tcp", ln.Addr().String())
-	defer dialer.Close()
 	if err != nil {
 		fmt.Println("Error dialing: ", err.Error())
 		os.Exit(1)
 	}
+	defer dialer.Close()
 
 	var info syscall.TCPInfo
 	b.ResetTimer()
