62a7d42f91d42398db98b6fbdc962e213a91c2d1
Simone Basso <bassosimone@gmail.com>
2018-02-27 15:43:15 +0100

    cleanup(u/io.go): remove unneeded code
    Since we're now using code in netx, we can clean up code
    that we don't use from the utils/io.go file.

--- util/io.go
+++ util/io.go
@@ -8,81 +8,12 @@ package util
 // See <https://groups.google.com/forum/#!topic/golang-nuts/afgEYsoV8j0>
 
 import (
-	"bufio"
 	"errors"
-	"io"
 	"net"
 	"time"
 )
 
 const IoTimeout = 10.0 * time.Second
-
-func IoReadFull(conn net.Conn, reader io.Reader, data []byte) (int, error) {
-	count := 0
-	err := conn.SetReadDeadline(time.Now().Add(IoTimeout))
-	if err != nil {
-		return count, err
-	}
-	count, err = io.ReadFull(reader, data)
-	if err != nil {
-		return count, err
-	}
-	err = conn.SetReadDeadline(time.Time{})
-	return count, err
-}
-
-func IoWriteByte(conn net.Conn, writer *bufio.Writer, data byte) error {
-	err := conn.SetWriteDeadline(time.Now().Add(IoTimeout))
-	if err != nil {
-		return err
-	}
-	err = writer.WriteByte(data)
-	if err != nil {
-		return err
-	}
-	return conn.SetWriteDeadline(time.Time{})
-}
-
-func IoWrite(conn net.Conn, writer io.Writer, data []byte) (int, error) {
-	count := 0
-	err := conn.SetWriteDeadline(time.Now().Add(IoTimeout))
-	if err != nil {
-		return count, err
-	}
-	count, err = writer.Write(data)
-	if err != nil {
-		return count, err
-	}
-	err = conn.SetWriteDeadline(time.Time{})
-	return count, err
-}
-
-func IoFlush(conn net.Conn, writer *bufio.Writer) error {
-	err := conn.SetWriteDeadline(time.Now().Add(IoTimeout))
-	if err != nil {
-		return err
-	}
-	err = writer.Flush()
-	if err != nil {
-		return err
-	}
-	return conn.SetWriteDeadline(time.Time{})
-}
-
-func IoWriteString(conn net.Conn, writer *bufio.Writer, data string) (
-	int, error) {
-	count := 0
-	err := conn.SetWriteDeadline(time.Now().Add(IoTimeout))
-	if err != nil {
-		return count, err
-	}
-	count, err = writer.WriteString(data)
-	if err != nil {
-		return count, err
-	}
-	err = conn.SetWriteDeadline(time.Time{})
-	return count, err
-}
 
 func IoAccept(listener net.Listener) (net.Conn, error) {
 	tcp_listener, okay := listener.(*net.TCPListener)
